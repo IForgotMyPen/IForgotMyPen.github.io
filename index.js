1a01fbdcc5eb531b856197beac2d0478159e28b8
Brendan Birschbach <brendanbirsc@gmail.com>
2026-07-05 17:46:33 -0500

    Added JS code for switching tab content.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function changeTab(newTabId) {
+    // Change the button styling to indicate which tab is active.
     document.querySelectorAll('.tab').forEach((tab) => tab.classList.remove('active-tab'));
+    document.querySelector('#tab-bar').querySelector(`#${newTabId}`).classList.add('active-tab');
 
-    document.querySelector(`#${newTabId}`).classList.add('active-tab');
+    // Change the tab content to match the currently active tab.
+    document.querySelectorAll('.tab-content').forEach((tab) => tab.classList.remove('active-tab-content'));
+    document.querySelector('#tab-content-area').querySelector(`#${newTabId}`).classList.add('active-tab-content');
 }
